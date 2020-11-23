e5aec9b32fbd889a9e2b6bc0afa64db4721a2f1e
Evhen-k-byte <gekanator2@gmail.com>
2020-11-23 11:50:37 +0000

    add second menu (sidebar)

--- js/script.js
+++ js/script.js
@@ -19,22 +19,6 @@
 })();
 /* плавная прокрутка на 100vh */
 
-/* плавный скрол для меню */
-const anchors = document.querySelectorAll('a[href*="#"]')
-anchors.forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    const blockID = anchor.getAttribute('href').substr(1)
-    $('html, body').animate({
-      scrollTop: $(document.getElementById(blockID)).offset().top
-    }, 1000);
-    stopScroll();
-    closeMenu();
-    scroll();
-  })
-})
-/* плавный скрол для меню */
-
 /* автозакрытие меню */
 function closeMenu() {
   document.querySelector('#toggle').click();
@@ -43,7 +27,6 @@ function closeMenu() {
 
 /* запрет прокрутки */
 var body = document.getElementById("body");
-
 function stopScroll() {
   body.classList.toggle("stop-scroll");
 }
@@ -54,3 +37,76 @@ function scroll() {
   body.classList.toggle("scroll");
 }
 /* включение прокрутки */
+
+/* плавный скрол для меню */
+var isBurger = true;
+const anchors = document.querySelectorAll('a[href*="#"]')
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const blockID = anchor.getAttribute('href').substr(1)
+    $('html, body').animate({
+      scrollTop: $(document.getElementById(blockID)).offset().top
+    }, 1000);
+    if (isBurger) {
+      closeMenu();
+      scroll();
+    }
+    else {
+      closeNav();
+      arrow.classList.remove("animation-target__splat");
+      scroll();
+      isMenuShow = true;
+    }
+
+  })
+})
+/* плавный скрол для меню */
+
+/* манипуляции с переключателем меню */
+var changer = document.getElementById("changer");
+var toggleContainer = document.getElementById("toggleContainer");
+var toggleLeft = document.getElementById("toggle-left");
+var arrow = document.getElementById("arrow");
+
+
+function Rotate() {
+  changer.classList.toggle("animation-target__rotate");
+  toggleContainer.classList.toggle("toggler-change");
+  toggleLeft.classList.toggle("toggle-left-visible");
+
+  function removeRotate() {
+    changer.classList.remove("animation-target__rotate");
+  }
+  setTimeout(removeRotate, 1200);
+}
+
+/* открыть/закрыть боковое меню */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  isBurger = false;
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  isBurger = true;
+}
+/* открыть/закрыть боковое меню */
+
+/* следить за кликами*/
+var isMenuShow = true;
+toggleLeft.addEventListener("click", function () {
+  arrow.classList.toggle("animation-target__splat");
+  if (isMenuShow) {
+    openNav();
+    stopScroll();
+    isMenuShow = false;
+  }
+  else {
+    closeNav();
+    scroll();
+    isMenuShow = true;
+  }
+});
+/* следить за кликами*/
+
+/* манипуляции с переключателем меню */
